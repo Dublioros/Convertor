18361489edd33bc69d13d4c2bb32e58ff4fcfc48
Jose Carlo Gutierrez Schweitzer <129911770+Dublioros@users.noreply.github.com>
2023-07-27 12:04:13 -0500

    Initial commit

---
+++ src/com/alura/conversorchallenge/latam/DistWindow.java
@@ -0,0 +1,2 @@
+package com.alura.conversorchallenge.latam;public class DistWindow {
+}
---
+++ src/com/alura/conversorchallenge/latam/TimeZoneWindow.java
@@ -0,0 +1,10 @@
+package com.alura.conversorchallenge.latam;
+
+import javax.swing.*;
+
+public class TZoneWindow {
+    private JComboBox<String> comboBoxTimeZone;
+    private JLabel jLabelTime;
+
+
+}
